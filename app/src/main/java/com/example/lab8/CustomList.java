c45aa3fb92c50878a8e4a7c8be9380b5e645729d
taekwan <taekwan@ualberta.ca>
2024-10-24 20:14:28 -0600

    implemented countCities

--- app/src/main/java/com/example/lab8/CustomList.java
+++ app/src/main/java/com/example/lab8/CustomList.java
@@ -66,4 +66,7 @@ public class CustomList extends ArrayAdapter<City> {
     public void deleteCity(City city) {
         cities.remove(city);
     }
+    public int countCities() {
+        return cities.size();
+    }
 }
